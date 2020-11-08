8e6528087aeeac20914e4701624d27d2419fdc45
jaime.hernandez <jaime.hernandez@bi-geek.com>
2020-11-08 20:22:41 +0100

    Added suite

---
+++ src/test/java/usantatecla/draughts/models/AllModels.java
@@ -0,0 +1,10 @@
+package usantatecla.draughts.models;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({TurnTest.class, StateTest.class, CoordinateTest.class})
+public class AllModels {
+}
